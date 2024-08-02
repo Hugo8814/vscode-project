2b1d14b50ea89506e0537c979f4f46c79e5ca137
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-02 20:34:26 +0100

    okay

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,67 +1,77 @@
 import styles from "../styles/Projects.module.css";
 import audiophile from "../img/audiophile.png";
-import { useState } from "react";
 import githubSvg from "../components/icons/github.svg";
 import linkSvg from "../components/icons/link.svg";
+import countryApi from "../img/countryApi.png";
 
 function Projects() {
-  const [hover, setHover] = useState(false);
   return (
     <div className={styles.container}>
       <div className={styles.projects}>
         <div className={styles.title}>Audiophile e-com</div>
-        <div
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          className={styles.box}
-        >
+        <div className={styles.box}>
           <img
-            className={`${styles.projectsImg} ${hover ? styles.blur : ""}`}
+            className={styles.projectsImg}
             src={audiophile}
             alt="project img"
           />
-          {hover && (
-            <div className={styles.overlay}>
-              <a href="#" className="btn">
-                <img src={linkSvg} alt="github logo" className={styles.link} />
-              </a>
-              <a href="#" className="btn">
-                <img
-                  src={githubSvg}
-                  alt="github logo"
-                  className={styles.github}
-                />
-              </a>
-            </div>
-          )}
+          <div className={styles.overlay}>
+            <a href="#" className="btn">
+              <img src={linkSvg} alt="link logo" className={styles.link} />
+            </a>
+            <a href="#" className="btn">
+              <img
+                src={githubSvg}
+                alt="github logo"
+                className={styles.github}
+              />
+            </a>
+          </div>
         </div>
       </div>
       <div className={styles.projects}>
         <div className={styles.title}>Countries Api</div>
-        <div
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          className={styles.box}
-        >
+        <div className={styles.box}>
           <img
-            className={`${styles.projectsImg} ${hover ? styles.blur : ""}`}
-            src={audiophile}
+            className={styles.projectsImg}
+            src={countryApi}
             alt="project img"
           />
-          {hover && (
-            <div className={styles.overlay}>
-              <a href="#" className="btn">
-                <img src={linkSvg} alt="github logo" className={styles.link} />
-              </a>
-              <a href="#" className="btn">
-                <img
-                  src={githubSvg}
-                  alt="github logo"
-                  className={styles.github}
-                />
-              </a>
-            </div>
-          )}
+          <div className={styles.overlay}>
+            <a href="#" className="btn">
+              <img src={linkSvg} alt="link logo" className={styles.link} />
+            </a>
+            <a href="#" className="btn">
+              <img
+                src={githubSvg}
+                alt="github logo"
+                className={styles.github}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.projects}>
+        <div className={styles.title}>Countries Api</div>
+        <div className={styles.box}>
+          <img
+            className={styles.projectsImg}
+            src={countryApi}
+            alt="project img"
+          />
+          <div className={styles.overlay}>
+            <a href="#" className="btn">
+              <img src={linkSvg} alt="link logo" className={styles.link} />
+            </a>
+            <a href="#" className="btn">
+              <img
+                src={githubSvg}
+                alt="github logo"
+                className={styles.github}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
