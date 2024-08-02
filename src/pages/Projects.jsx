ebe13b5647840c97629952c9df3492d5c2808c45
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-02 19:04:51 +0100

    okay

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,34 @@
 import styles from "../styles/Projects.module.css";
 import audiophile from "../img/audiophile.png";
+import { useState } from "react";
 
 function Projects() {
+  const [hover, setHover] = useState(false);
   return (
     <div className={styles.container}>
       <div className={styles.projects}>
         <div className={styles.title}>Audiophile e-com</div>
-        <img
-          className={styles.projectsImg}
-          src={audiophile}
-          alt="project img"
-        />
+        <div
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          className={styles.box}
+        >
+          <img
+            className={`${styles.projectsImg} ${hover ? styles.blur : ""}`}
+            src={audiophile}
+            alt="project img"
+          />
+          {hover && (
+            <div className={styles.overlay}>
+              <a href="#" className="btn">
+                Link 1
+              </a>
+              <a href="#" className="btn">
+                Link 2
+              </a>
+            </div>
+          )}
+        </div>
       </div>
 
       <div className={styles.projects}>
