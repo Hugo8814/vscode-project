9bf472b957c6ff5618847069b66130b9ed35502c
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-02 20:17:31 +0100

    okay

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -36,46 +36,33 @@ function Projects() {
           )}
         </div>
       </div>
-
       <div className={styles.projects}>
-        <div className={styles.title}>Audiophile e-com</div>
-        <img
-          className={styles.projectsImg}
-          src={audiophile}
-          alt="project img"
-        />
-      </div>
-      <div className={styles.projects}>
-        <div className={styles.title}>Audiophile e-com</div>
-        <img
-          className={styles.projectsImg}
-          src={audiophile}
-          alt="project img"
-        />
-      </div>
-      <div className={styles.projects}>
-        <div className={styles.title}>Audiophile e-com</div>
-        <img
-          className={styles.projectsImg}
-          src={audiophile}
-          alt="project img"
-        />
-      </div>
-      <div className={styles.projects}>
-        <div className={styles.title}>Audiophile e-com</div>
-        <img
-          className={styles.projectsImg}
-          src={audiophile}
-          alt="project img"
-        />
-      </div>
-      <div className={styles.projects}>
-        <div className={styles.title}>Audiophile e-com</div>
-        <img
-          className={styles.projectsImg}
-          src={audiophile}
-          alt="project img"
-        />
+        <div className={styles.title}>Countries Api</div>
+        <div
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          className={styles.box}
+        >
+          <img
+            className={`${styles.projectsImg} ${hover ? styles.blur : ""}`}
+            src={audiophile}
+            alt="project img"
+          />
+          {hover && (
+            <div className={styles.overlay}>
+              <a href="#" className="btn">
+                <img src={linkSvg} alt="github logo" className={styles.link} />
+              </a>
+              <a href="#" className="btn">
+                <img
+                  src={githubSvg}
+                  alt="github logo"
+                  className={styles.github}
+                />
+              </a>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
