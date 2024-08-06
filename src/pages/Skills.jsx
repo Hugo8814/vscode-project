ea7653e624f08803a74e878e699e40f878a900de
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-06 15:52:23 +0100

    okay

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,15 +1,36 @@
 import styles from "../styles/skills.module.css";
 import jsimg from "../img/js.png";
 import jsUdemy from "../img/jsUdemy.jpg";
-import jsicon from "../img/jsicon.png";
 
+import jsSVG from "../components/icons/js.svg";
+import sassSVG from "../components/icons/sass.svg";
+import htmlSVG from "../components/icons/html-5.svg";
+import cssSVG from "../components/icons/css-3.svg";
+import reactSVG from "../components/icons/react.svg";
+import gitSVG from "../components/icons/git.svg";
 function Skills() {
   return (
     <div className={styles.skillsPage}>
       <div className={styles.skillsTitle}>Skills</div>
       <div className={styles.skillsGrid}>
         <div className={styles.skills}>
-          <img className={styles.skillsimg} src={jsicon} alt="" />
+          <img className={styles.skillsimg} src={htmlSVG} alt="" />
+        </div>
+        <div className={styles.skills}>
+          <img className={styles.skillsimg} src={cssSVG} alt="" />
+        </div>
+        <div className={styles.skills}>
+          <img className={styles.skillsimg} src={sassSVG} alt="" />
+        </div>
+        <div className={styles.skills}>
+          <img className={styles.skillsimg} src={jsSVG} alt="" />
+        </div>
+
+        <div className={styles.skills}>
+          <img className={styles.skillsimg} src={reactSVG} alt="" />
+        </div>
+        <div className={styles.skills}>
+          <img className={styles.skillsimg} src={gitSVG} alt="" />
         </div>
       </div>
 
