107c12a43a8487e88594854d4007844573d04b2d
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-06 15:15:19 +0100

    okay

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,35 @@
 import styles from "../styles/skills.module.css";
+import jsimg from "../img/js.png";
+import jsUdemy from "../img/jsUdemy.jpg";
+import jsicon from "../img/jsicon.png";
 
 function Skills() {
-  return <div className={styles.skillsPage}>Skills</div>;
+  return (
+    <div className={styles.skillsPage}>
+      <div className={styles.skillsTitle}>Skills</div>
+      <div className={styles.skillsGrid}>
+        <div className={styles.skills}>
+          <img className={styles.skillsimg} src={jsicon} alt="" />
+        </div>
+      </div>
+
+      <div className={styles.skillsTitle}>Certs</div>
+      <div className={styles.certsGrid}>
+        <div className={styles.certs}>
+          <img className={styles.certsimg} src={jsimg} alt="" />
+        </div>
+        <div className={styles.certs}>
+          <img className={styles.certsimg} src={jsUdemy} alt="" />
+        </div>
+        <div className={styles.certs}>
+          <img className={styles.certsimg} src={jsimg} alt="" />
+        </div>
+        <div className={styles.certs}>
+          <img className={styles.certsimg} src={jsimg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
