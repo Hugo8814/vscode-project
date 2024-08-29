6cec5a33b854b38d59f758f6bdd3c28d171e02de
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-29 10:56:48 +0100

    okay

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,12 +3,35 @@ import audiophile from "../img/audiophile.png";
 import githubSvg from "../components/icons/github.svg";
 import linkSvg from "../components/icons/link.svg";
 //import countryApi from "../img/countryApi.png";
-import img from "../img/img.png";
+
 import img2 from "../img/im2.png";
+import wmc from "../img/wmc.png";
 
 function Projects() {
   return (
     <div className={styles.container}>
+      <div className={styles.projects}>
+        <div className={styles.title}>Walfare MC</div>
+        <div className={styles.box}>
+          <img
+            className={`${styles.projectsImg} `}
+            src={wmc}
+            alt="project img"
+          />
+          <div className={styles.overlay}>
+            <a href="https://welfaremc.netlify.app/" className="btn">
+              <img src={linkSvg} alt="link logo" className={styles.link} />
+            </a>
+            <a href="https://github.com/Hugo8814/WMC" className="btn">
+              <img
+                src={githubSvg}
+                alt="github logo"
+                className={styles.github}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
       <div className={styles.projects}>
         <div className={styles.title}>Audiophile e-com</div>
         <div className={styles.box}>
@@ -21,10 +44,7 @@ function Projects() {
             <a href="https://audiophile-hugo.netlify.app/" className="btn">
               <img src={linkSvg} alt="link logo" className={styles.link} />
             </a>
-            <a
-              href="https://github.com/Hugo8814/vscode-project"
-              className="btn"
-            >
+            <a href="https://github.com/Hugo8814/audiophile" className="btn">
               <img
                 src={githubSvg}
                 alt="github logo"
@@ -46,29 +66,6 @@ function Projects() {
               href="https://github.com/Hugo8814/rest-countries-api-with-color-theme-switcher-master"
               className="btn"
             >
-              <img
-                src={githubSvg}
-                alt="github logo"
-                className={styles.github}
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.projects}>
-        <div className={styles.title}>Coming soon</div>
-        <div className={styles.box}>
-          <img
-            className={`${styles.projectsImg} ${styles.bulr}`}
-            src={img}
-            alt="project img"
-          />
-          <div className={styles.overlay}>
-            <a href="#" className="btn">
-              <img src={linkSvg} alt="link logo" className={styles.link} />
-            </a>
-            <a href="#" className="btn">
               <img
                 src={githubSvg}
                 alt="github logo"
