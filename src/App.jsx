2d610d31ee646581d702abc09f5577091d91768a
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-11-24 03:22:35 +0000

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,10 @@ import Audiophile from "./pages/Audiophile";
 import WelfareMC from "./pages/WelfareMC";
 import Countries from "./pages/Countries";
 
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
-function ScrollToTop() {
-    const { pathname } = useLocation();
-
-    useEffect(() => {
-        // Scroll to top whenever the path changes
-        window.scrollTo(0, 0);
-    }, [pathname]);
-
-    return null; // This component doesn't render anything
-}
 
 const App = () => (
   <Router>
-    <ScrollToTop /> {/* Place this here */}
+    
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-// useLocation from react-router-dom
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Titlebar from "./Titlebar";
 import Sidebar from "./SideBar";
 import Explorer from "./Explorer";
@@ -7,23 +8,38 @@ import Bottombar from "./Bottombar";
 import Tabsbar from "./Tabsbar";
 import styles from "../styles/Layout.module.css";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+      const mainContent = document.querySelector("#main-editor"); // Target your main content
+      if (mainContent) {
+          mainContent.scrollTo({ top: 0, behavior: "smooth" }); // Smoothly scroll the container
+      }
+  }, [pathname]);
+
+  return null; // This component doesn't render anything
+}
+
+
 const Layout = ({ children }) => {
-  return (
-    <>
-      <Titlebar />
-      <div className={styles.main}>
-        <Sidebar />
-        <Explorer />
-        <div style={{ width: "100%" }} className={styles.box}>
-          <Tabsbar />
-          <main id="main-editor" className={styles.content}>
-            {children}
-          </main>
-        </div>
-      </div>
-      <Bottombar />
-    </>
-  );
+    return (
+        <>
+            <Titlebar />
+            <div className={styles.main}>
+                <Sidebar />
+                <Explorer />
+                <div style={{ width: "100%" }} className={styles.box}>
+                    <Tabsbar />
+                    <ScrollToTop /> {/* Ensure ScrollToTop is rendered here */}
+                    <main id="main-editor" className={styles.content}>
+                        {children}
+                    </main>
+                </div>
+            </div>
+            <Bottombar />
+        </>
+    );
 };
 
 export default Layout;
